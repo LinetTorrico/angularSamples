884d08a2637bf63db4703d7af86bd0735f9c04f8
Linet Torrico <linet.torrico@dharbor.com>
2019-06-11 01:24:22 -0400

    Case accepted
    Case accepted

--- angular-sample/src/app/left-menu/left-menu.component.ts
+++ angular-sample/src/app/left-menu/left-menu.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-left-menu',
   templateUrl: './left-menu.component.html',
-  styleUrls: ['./left-menu.component.css']
+  styleUrls: ['./left-menu.component.scss']
 })
 export class LeftMenuComponent implements OnInit {
    isCaseAccepted=false;
@@ -12,6 +12,22 @@ export class LeftMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    if(this.noCaseAccepted){
+      console.log("ingreo");
+      //$('#step-indicator').addClass('progressbar');
+      this.addClass('step-indicator', 'noAccepted' )
+    }else{
+      $('#step-indicator').removeClass('noAccepted');
+    }
   }
+  addClass(className: string, addingClass){
+    const elements = document.getElementsByClassName(className) ;
 
+    let i=0;
+    while ( i<elements.length){
+      const e = elements[i];
+      e.classList.add(addingClass);
+      i++;
+    }
+  }
 }
