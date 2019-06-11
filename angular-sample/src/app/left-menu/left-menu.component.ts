f5cc5f937ea57d1656c3fb1176be464d9f69897b
Linet Torrico <linet.torrico@dharbor.com>
2019-06-11 01:39:34 -0400

    refactor

--- angular-sample/src/app/left-menu/left-menu.component.ts
+++ angular-sample/src/app/left-menu/left-menu.component.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 declare var $: any;
+
 @Component({
   selector: 'app-left-menu',
   templateUrl: './left-menu.component.html',
   styleUrls: ['./left-menu.component.scss']
 })
 export class LeftMenuComponent implements OnInit {
-   isCaseAccepted=false;
-   noCaseAccepted=false;
-  screeningCompleted=true;
-  appReviewCompleted=true;
-   fill=true;
-  constructor() { }
+  isCaseAccepted = false;
+  noCaseAccepted = false;
+  screeningCompleted = true;
+  appReviewCompleted = true;
+  fill = true;
+
+  constructor() {
+  }
 
   ngOnInit() {
-    if(this.noCaseAccepted){
-      console.log("ingreo");
-      //$('#step-indicator').addClass('progressbar');
-      this.addClass('step-indicator', 'noAccepted' )
-    }else if(!this.noCaseAccepted && this.screeningCompleted && this.appReviewCompleted){
-      this.addClass('step-indicator', 'caseAccepted' )
-    }else{
-      $('#step-indicator').removeClass('noAccepted');
+    if (this.noCaseAccepted) {
+      this.addClass('step-indicator', 'noAccepted')
+    } else if (!this.noCaseAccepted && this.screeningCompleted && this.appReviewCompleted) {
+      this.addClass('step-indicator', 'caseAccepted')
+    } else {
     }
   }
-  addClass(className: string, addingClass){
-    const elements = document.getElementsByClassName(className) ;
 
-    let i=0;
-    while ( i<elements.length){
+  addClass(className: string, addingClass) {
+    const elements = document.getElementsByClassName(className);
+    let i = 0;
+    while (i < elements.length) {
       const e = elements[i];
       e.classList.add(addingClass);
       i++;
